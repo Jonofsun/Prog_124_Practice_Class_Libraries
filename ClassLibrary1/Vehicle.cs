61310c8781e61fd1db5c149831c2ffbe207ae598
Jonofsun <jonofsun77@live.com>
2023-06-08 17:30:49 -0700

    Initial commit

---
+++ ClassLibrary1/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Vehicle
+    {
+        string _color;
+
+        public Vehicle(string color)
+        {
+            _color = color;
+        }
+
+        public string Color { get => _color; set => _color = value; }
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} - {Color}";
+        }
+    }
+}
---
+++ Prog_124_Practice_Class_Libraries/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary1;
+using System.Numerics;
+
+namespace Prog_124_Practice_Class_Libraries
+{
+    internal class Program
+    {
+        static List<Vehicle> vehicles = new List<Vehicle>();
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle("Red");
+            Air air = new Air("Green");
+            Helicopter helicopter = new Helicopter("Blue", 2);
+            ClassLibrary1.Plane plane = new ClassLibrary1.Plane("Pink", 3);
+            Sea sea = new Sea("Orange", "Submarine");
+            Land land = new Land("Purple", 4);
+
+            vehicles.Add(vehicle);
+            vehicles.Add(air);
+            vehicles.Add(helicopter);
+            vehicles.Add(plane);
+            vehicles.Add(sea);
+            vehicles.Add(land);
+
+            foreach (Vehicle v in vehicles)
+            {
+                Console.WriteLine(v);
+            }
+
+        }
+    }
+}
